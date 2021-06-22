cc67cec8d9578dfed4aeff9b4fb3e1788096148e
Andrew George Issac <andrewgeorgeissac@gmail.com>
2021-06-22 20:33:45 +0530

    v1.0

--- lunar-weather/app.js
+++ lunar-weather/app.js
@@ -5,15 +5,19 @@ const app=express();
 
 app.set("view engine","ejs");
 app.use(body_parser.urlencoded({extended:true}));
+app.use(body_parser.json());
 app.use(express.static("public"));
 
 app.get("/",(req,res)=>{
     var today=new Date();
     var Day="Monday";
     res.render("index");
+    console.log(req.body.name);
 })
 
-
+app.post("/",(req,res)=>{
+    console.log(req.body.name);
+})
 app.listen(3000,()=>{
     console.log("running @ 3000");
 })
