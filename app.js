19a3df3939317f29b3262a62b2961f247616c023
Andrew George Issac <andrewgeorgeissac@gmail.com>
2021-06-26 22:02:20 +0530

    v1.0

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express=require("express");
 const body_parser=require("body-parser");
 const https=require("https");
+const http=require("http");
 var latitude;
 var longitude;
-var location="";
-var temp="";
-var img_url="",description="",windspeed=0;
+var location="",location1="Hong Kong",location3="London";
+var temp="",temp1="",temp3="";
+var img_url="",img_url1="",img_url3="";
+var description="",description1="",description3="";
+var windspeed=0,windspeed1=0,windspeed3=0;
+const apikey="6ec36b52be3e1347f1cf9bf07a73c37d";
+const units="metric";
+const url1="http://api.openweathermap.org/data/2.5/weather?q="+location1+"&appid="+apikey+"&units="+units;
+const url3="http://api.openweathermap.org/data/2.5/weather?q="+location3+"&appid="+apikey+"&units="+units;
+
 const app=express();
 
 app.set("view engine","ejs");
@@ -13,8 +21,27 @@ app.use(body_parser.urlencoded({extended:true}));
 app.use(body_parser.json());
 app.use(express.static("public"));
 
+
+
 app.get("/",(req,res)=>{
-    res.render("index",{Location:location,Temp:temp,Img_url:img_url,Description:description,WindSpeed:windspeed});
+    const url1="http://api.openweathermap.org/data/2.5/weather?q="+location1+"&appid="+apikey+"&units="+units;
+    http.get(url1,function(response){
+        //console.log(response);
+        response.on("data",function(data){
+            const weatherData=JSON.parse(data);
+            temp1=weatherData.main.temp;
+            description1=weatherData.weather[0].description;
+            location1=weatherData.name;
+            const icon=weatherData.weather[0].icon;
+            windspeed1=parseInt(weatherData.wind.speed)*3.6;
+            img_url1="http://openweathermap.org/img/wn/"+icon+"@2x.png";
+            console.log("BOX1:",temp1,description1,location1);
+            
+        })   
+    })
+
+
+    res.render("index",{Location:location,Temp:temp,Img_url:img_url,Description:description,WindSpeed:windspeed,Location1:location1,Temp1:temp1,Img_url1:img_url1,Description1:description1,WindSpeed1:windspeed1});
 })
 
 app.post("/",(req,res)=>{ 
@@ -22,10 +49,7 @@ app.post("/",(req,res)=>{
     longitude=req.body.longitude;
     console.log(req.body.latitude);
     console.log(req.body.longitude);
-    const apikey="6ec36b52be3e1347f1cf9bf07a73c37d";
-    const units="metric";
     const url="https://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude+"&appid="+apikey+"&units="+units;
-    
     https.get(url,function(response){
         //console.log(response);
         response.on("data",function(data){
